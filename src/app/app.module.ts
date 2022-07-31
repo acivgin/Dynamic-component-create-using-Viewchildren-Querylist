c90ccc00ddbd77295ed1bdf52397dba88ab1645e
Adis Civgin <civgin.adis@hotmail.com>
2022-07-31 18:07:39 +0200

    fisrt commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { ExpandedSectionComponent } from './expanded-section/expanded-section.component';
-import { DatacontainerDirective } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-
+import { AppComponent } from "./app.component";
+import { ExpandedSectionComponent } from "./expanded-section/expanded-section.component";
+import { DataContainerDirective } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
     ExpandedSectionComponent,
-    DatacontainerDirective
+    DataContainerDirective,
   ],
   providers: [],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    CommonModule
-  ],
-  entryComponents: [
-    ExpandedSectionComponent
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, HttpClientModule, FormsModule, CommonModule],
+  entryComponents: [ExpandedSectionComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
